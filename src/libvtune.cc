208a7c56b2577a928432ee02febf8184f41612a9
yanghe1 <he.yang@intel.com>
2019-05-25 03:11:48 +0800

    version 0.2

--- src/libvtune.cc
+++ src/libvtune.cc
@@ -5,24 +5,44 @@
 
 int invoke(const char* params) {
 
-  int result = 0;
+  int errcode = 0;
 
   static __itt_domain* domain = NULL;
 
   if(strncmp(params, "init:", 5) == 0) {
-	  domain = __itt_domain_create(params + 5);
+    if(*(params+5)) {
+      domain = __itt_domain_create(params + 5);
+      if(!domain) {
+        errcode += CREATE_DOMAIN_FAILED;
+      }
+    } else {
+      errcode += NO_DOMAIN_NAME;
+    }
   } else if (strncmp(params, "start:", 6) == 0) {
-    __itt_string_handle* handle_start = __itt_string_handle_create(params + 6);
-	  __itt_task_begin(domain, __itt_null, __itt_null, handle_start);
+    if(*(params + 6)) {
+      __itt_string_handle* handle_start = __itt_string_handle_create(params + 6);
+      if(handle_start) {
+        __itt_task_begin(domain, __itt_null, __itt_null, handle_start);
+      } else {
+        errcode += CREATE_HANDLE_FAILED;
+      }
+      
+    } else {
+      errcode += NO_HANDLE_NAME;
+    }
   } else if (strncmp(params, "end", 3) == 0) {
 	  __itt_task_end(domain);
   } else if (strncmp(params, "name:", 5) == 0) {
-    __itt_thread_set_name(params + 5);
+    if(*(params + 5)) {
+      __itt_thread_set_name(params + 5);
+    } else {
+      errcode += SET_THREAD_NAME_FAILED;
+    }
   } else if (strncmp(params, "ignore", 6) == 0) {
-	  __itt_thread_ignore();
+    __itt_thread_ignore();
   } else {
-	  result = 1;
+	  errcode += UNKNOWN_PARAMS;
   }
 
-  return result;
+  return errcode;
 }
--- src/libvtune.h
+++ src/libvtune.h
@@ -1,6 +1,16 @@
 #ifndef _LIB_VTUNE_H_
 #define _LIB_VTUNE_H_
 
+#define UNKNOWN_PARAMS       1<<0
+#define NO_DOMAIN_NAME       1<<1
+#define CREATE_DOMAIN_FAILED 1<<2
+#define NO_HANDLE_NAME       1<<3
+#define CREATE_HANDLE_FAILED 1<<4
+#define TASK_BEGIN_FAILED    1<<5
+#define TASK_END_FAILED      1<<6
+#define SET_THREAD_NAME_FAILED 1<<7
+#define IGNORE_THREAD_FAILED 1<<8
+
 extern "C" {
   extern int invoke(const char* params);
 }
