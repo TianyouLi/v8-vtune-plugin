8e16c6e21453276a0a519dfa9ef540d0da2d5133
Tianyou Li <tianyou.li@intel.com>
2019-05-26 12:36:43 +0800

    interface

---
+++ src/vtuneapi.cc
@@ -0,0 +1,75 @@
+#include "vtuneapi.h"
+
+std::map<std::string, std::shared_ptr<VTuneDomain>> VTuneDomain::domains_;
+std::map<std::string, __itt_string_handle*> VTuneDomain::string_handlers_;
+
+
+std::shared_ptr<VTuneDomain> VTuneDomain::createDomain(const char* domain_name)
+{
+  auto domain = getDomain(domain_name);
+  
+  if (domain == nullptr) {
+    __itt_domain* itt_domain = NULL; // call api
+    if (itt_domain != NULL) {
+      std::string key(domain_name);
+      std::shared_ptr<VTuneDomain> value(new VTuneDomain(itt_domain));
+      domains_.insert(std::make_pair(key, value));
+    }
+  }
+
+  return domain;
+}
+
+void VTuneDomain::destroyDomain(const char* domain_name)
+{
+  auto it = domains_.find(domain_name);
+  if (it != domains_.end()) {
+    domains_.erase(it);
+  }
+}
+
+std::shared_ptr<VTuneDomain> VTuneDomain::getDomain(const char* domain_name)
+{
+  std::shared_ptr<VTuneDomain> result(nullptr) ;
+  
+  auto it = domains_.find(domain_name);
+  if (it!= domains_.end()) {
+    result = it->second;
+  }
+
+  return result;
+}
+
+__itt_string_handle* VTuneDomain::getString(const char* str)
+{
+  __itt_string_handle* result = NULL;
+  
+  auto it = string_handlers_.find(str);
+  if (it != string_handlers_.end()) {
+    result = it->second;
+  } else {
+    result = NULL; // call api
+    std::string key(str);
+    string_handlers_.insert(std::make_pair(key, result));
+  }
+
+  return result;
+}
+
+bool VTuneDomain::beginTask(const char* task_name)
+{
+  bool result = false;
+  
+  __itt_string_handle* name = getString(task_name);
+  if (name != NULL) {
+    //    __itt_task_begin(domain_, __itt_null, __itt_null, name);
+    result = true;
+  }
+  
+  return result;
+}
+
+void VTuneDomain::endTask()
+{
+  //  __itt_task_end(domain_);
+}
