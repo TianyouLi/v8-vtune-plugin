157846de8a426c2b69c520f5e0ed64869bcaa060
yanghe1 <he.yang@intel.com>
2019-05-29 07:52:43 +0800

    version 0.6

--- build/build.sh
+++ build/build.sh
@@ -2,6 +2,8 @@
 CUR_DIR=`pwd`
 BLD_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+export INTEL_VTUNE=/opt/intel/vtune_amplifier_2019.3.0.590814
+
 cd ${BLD_DIR}
 cmake ${BLD_DIR}
 make
--- src/libvtune.h
+++ src/libvtune.h
@@ -1,8 +1,25 @@
 #ifndef _LIB_VTUNE_H_
 #define _LIB_VTUNE_H_
 
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int startTask(vector<string>& vparams);
+int endTask(vector<string>& vparams);
+
+#define UNKNOWN_PARAMS       1<<0
+#define NO_DOMAIN_NAME       1<<1
+#define CREATE_DOMAIN_FAILED 1<<2
+#define NO_TASK_NAME       1<<3
+#define CREATE_TASK_FAILED 1<<4
+#define TASK_BEGIN_FAILED    1<<5
+#define TASK_END_FAILED      1<<6
+
 extern "C" {
   extern int invoke(const char* params);
 }
 
 #endif
+
